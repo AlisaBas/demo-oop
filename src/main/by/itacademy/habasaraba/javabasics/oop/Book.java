53cfaa9f69ed8626a015e31d9ef297f793e9cf92
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-09 19:35:48 -0500

    book class added with gets and sets

---
+++ src/main/by/itacademy/habasaraba/javabasics/oop/Book.java
@@ -0,0 +1,70 @@
+package by.itacademy.habasaraba.javabasics.oop;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private String publisher;
+
+    private int year;
+
+    private int pages;
+    private int price;
+
+    public void setId(int newId){
+        id=newId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String newName){
+        name=newName;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAuthor(String newAuthor){
+        author=newAuthor;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setPublisher(String newPublisher){
+        publisher=newPublisher;
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+
+    public void setYear(int newYear){
+        year=newYear;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setPages(int newPages){
+        pages=newPages;
+    }
+
+    public int getPages(){
+        return pages;
+    }
+
+    public void setPrice(int newPrice){
+        price=newPrice;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+}
