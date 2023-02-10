56dac0aa8b7d5d71f115e42a3e6bcc582551745e
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-09 19:43:13 -0500

    car class added with sets and gets

--- src/main/by/itacademy/habasaraba/javabasics/oop/Car.java
+++ src/main/by/itacademy/habasaraba/javabasics/oop/Car.java
@@ -1,4 +1,68 @@
 package by.itacademy.habasaraba.javabasics.oop;
 
 public class Car {
+    private int id;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private int number;
+
+    public void setId(int newId){
+        id=newId;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setBrand(String newBrand){
+        brand=newBrand;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public void setModel(String newModel){
+        model=newModel;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setYear(int newYear){
+        year=newYear;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setColor(String newColor){
+        color=newColor;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setPrice(int newPrice){
+        price=newPrice;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setNumber(int newNumber){
+        number=newNumber;
+    }
+
+    public int getNumber(){
+        return number;
+    }
 }
+
