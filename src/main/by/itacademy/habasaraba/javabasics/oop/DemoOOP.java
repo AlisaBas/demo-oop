848114e4424bb508a30632844971ab8365858e8c
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-09 20:20:44 -0500

    added with sets and gets

--- src/main/by/itacademy/habasaraba/javabasics/oop/DemoOOP.java
+++ src/main/by/itacademy/habasaraba/javabasics/oop/DemoOOP.java
@@ -53,5 +53,16 @@ public class DemoOOP {
         System.out.println(carOne.getPrice());
         System.out.println(carOne.getNumber());
 
+        Customer customerOne=new Customer();
+        String name = faker.name().fullName(); // Miss Samanta Schmidt
+        String firstName = faker.name().firstName(); // Emory
+        String lastName = faker.name().lastName(); // Barton
+        String streetAddress = faker.address().streetAddress();// 60018 Sawayn Brooks Suite 449
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+
+
     }
 }
